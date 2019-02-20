88d52bd79e43c157919efeea63ed9e2efc7f4693
killer <wolfking068@gmail.com>
2019-02-20 10:15:54 -0800

    pause menu

---
+++ Assets/Resources/Harley/code/gamemanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanger : MonoBehaviour {
+    public static gamemanger GM;
+    public bool pause;
+    // Use this for initialization
+    void Awake()
+    {
+        if (GM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            GM = this;
+        }
+        else if (GM != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Resources/Harley/code/pause_menu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class pause_menu : MonoBehaviour {
+    public GameObject me;
+    public ThirdPerson look;
+    public Slider xlook;
+    public Slider ylook;
+    // Use this for initialization
+    void Start () {
+        xlook.onValueChanged.AddListener(delegate { xlooknow(); });
+        ylook.onValueChanged.AddListener(delegate { ylooknow(); });
+    }
+
+    // Update is called once per frame
+    public void manu()
+    {
+        gamemanger.GM.pause = true;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void game()
+    {
+        me.SetActive(false);
+        gamemanger.GM.pause = false;
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void xlooknow()
+    {
+        PlayerPrefs.SetFloat("xlook", xlook.value);
+        look.SensivityX = PlayerPrefs.GetFloat("xlook");
+    }
+    public void ylooknow()
+    {
+        PlayerPrefs.SetFloat("ylook", ylook.value);
+        look.SensivityY= PlayerPrefs.GetFloat("ylook");
+    }
+}
