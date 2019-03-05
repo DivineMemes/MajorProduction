2dd1897e181a8f2ccb450e6105311dbfb8bb862c
Wyatt Gallagher <s179053@students.aie.edu.au>
2019-03-05 14:42:30 -0800

    added teleporting cultist AI groundwork

--- Assets/Resources/Wyatt/Scripts/SoundTraps/PlaySound.cs
+++ Assets/Resources/Wyatt/Scripts/SoundTraps/PlaySound.cs
@@ -4,35 +4,49 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
-    AudioSource bruh;
-    AudioClip bruhClip;
+    //AudioSource bruh;
+    //AudioClip bruhClip;
     public SphereCollider soundCollider;
+    public float killTime;
+    bool alreadyKilling;
 
 
     private void Start()
     {
-        bruh = gameObject.GetComponent<AudioSource>();
-        bruhClip = bruh.clip;
+        alreadyKilling = false;
+    //  bruh = gameObject.GetComponent<AudioSource>();
+    //  bruhClip = bruh.clip;
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        bruh.PlayOneShot(bruhClip);
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //  bruh.PlayOneShot(bruhClip);
+    //}
 
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-
         {
             SphereCollider spawnedCollider = Instantiate(soundCollider, gameObject.transform.position, Quaternion.identity);
             spawnedCollider.tag = "Sound";
-
+            if(!alreadyKilling)
+            {
+                StartCoroutine(killCollider(spawnedCollider));
+            }
             //Destroy(gameObject);
-            Destroy(spawnedCollider);
-            AudioSource.PlayClipAtPoint(bruhClip, gameObject.transform.position);
+            
+            //Destroy(spawnedCollider);
+            //AudioSource.PlayClipAtPoint(bruhClip, gameObject.transform.position);
         }
 
         
+    }
+
+    IEnumerator killCollider(Collider collider)
+    {
+        alreadyKilling = true;
+        yield return new WaitForSeconds(killTime);
+        Destroy(collider);
+        alreadyKilling = false;
     }
 }
