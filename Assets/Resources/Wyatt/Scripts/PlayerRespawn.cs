911e735efda6e012c9085ecaf64cfc7bc646b4b0
Wyatt Gallagher <s179053@students.aie.edu.au>
2019-03-12 10:38:50 -0700

    adding checkpoint system

---
+++ Assets/Resources/Wyatt/Scripts/PlayerRespawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    Vector3 respawnPosition;
+    void Start()
+    {
+        respawnPosition = gameObject.transform.position;
+    }
+
+    private void Update()
+    {
+        //Respawn();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer.Equals(16))
+        {
+            respawnPosition = collision.gameObject.transform.position;
+            Destroy(collision.gameObject);
+        }
+    }
+    
+    void Respawn()
+    {   
+        //do respawn when you die
+    }
+
+}
