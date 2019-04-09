ee28e386c1810b1f0327819638302dd4b7721567
wolfking <wolfking068@gmail.com>
2019-04-09 10:40:22 -0700

    fixis

---
+++ Assets/Resources/Harley/code/win.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class win : MonoBehaviour {
+    public float raylength = 10;
+    RaycastHit hit;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawLine(transform.position, transform.position + transform.up, Color.green);
+        if (Physics.Raycast(transform.position, transform.position + transform.up, out hit, raylength))
+        {
+
+            if(hit.collider.tag == "Player")
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                //print("hit me");
+                SceneManager.LoadScene("you win");
+            }
+        }
+   }
+}
