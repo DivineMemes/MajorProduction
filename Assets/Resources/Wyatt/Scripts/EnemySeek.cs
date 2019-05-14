13077e9a59590dbd8c7901df01be7b6fd5a68b74
Wyatt Gallagher <s179053@students.aie.edu.au>
2019-05-14 12:45:11 -0700

    fixed enemy AI up and finished the flashlight functions and added more sounds

--- Assets/Resources/Wyatt/Scripts/EnemySeek.cs
+++ Assets/Resources/Wyatt/Scripts/EnemySeek.cs
@@ -7,7 +7,7 @@ public class EnemySeek : MonoBehaviour
     public Transform Flashlight;
     Vector3 velocity;
 
-    public bool flashlightSeen = false;
+    public bool flashLightOn = false;
     public bool targetSpotted = false;
     public bool targetWasSpotted = false;
     public bool sound;
@@ -16,6 +16,7 @@ public class EnemySeek : MonoBehaviour
     public AudioClip Spotted;
     AudioSource source;
 
+    public float FlashlightViewRad;
     public float viewRad; //enemies range of sight
     float s_ViewRad;//s_ == script value
     [Range(0, 360)]
@@ -49,61 +50,60 @@ public class EnemySeek : MonoBehaviour
         {
             s_ViewRad = viewRad;
         }
-        for(int i = 0; i < visibleTargets.Count; i++)
-        {
-            if (visibleTargets[i]==target)
-            {
-                targetSpotted = true;
-                if(!sound)
-                {
-                    source.PlayOneShot(Spotted);
-                    sound = true;
-                }
-                targetWasSpotted = true;
-                transform.LookAt(visibleTargets[i]);
-            }
 
-            if (visibleTargets[i] == Flashlight && visibleTargets[i] != target)
-            {
-                flashlightSeen = true;
-            }
-        }
-        if(!visibleTargets.Contains(Flashlight))
-        {
-            flashlightSeen = false;
-        }
-        if(visibleTargets.Count == 0)
+        // if we haven't sensed anything
+        if (visibleTargets.Count == 0)
         {
             targetSpotted = false;
-            flashlightSeen = false;
+            flashLightOn = false;
             sound = false;
+        }
+        // if we have...
+        else
+        {
+            for (int i = 0; i < visibleTargets.Count; i++)
+            {
+                if (visibleTargets[i] == target)
+                {
+                    targetSpotted = true;
+                    if (!sound)
+                    {
+                        source.PlayOneShot(Spotted);
+                        sound = true;
+                    }
+                    targetWasSpotted = true;
+                    transform.LookAt(visibleTargets[i]);
+                }
+            }
+
+            flashLightOn = !visibleTargets.Contains(target) && visibleTargets.Contains(Flashlight);
         }
     }
     void FindVisibleTargets()
     {
-        Collider[] targetsInView = Physics.OverlapSphere(transform.position, s_ViewRad, targetMask);//create detection sphere
-        Collider[] flashlightInView = Physics.OverlapSphere(new Vector3(transform.position.x, transform.position.y+1, transform.position.z), flashLightRad, targetMask);
         visibleTargets.Clear();
+        Collider[] targetsInView = Physics.OverlapSphere(transform.position, s_ViewRad, targetMask);//create detection sphere
         for (int i = 0; i < targetsInView.Length; i++)
         {
-            for(int j = 0; j < flashlightInView.Length; j++)
+            Transform target = targetsInView[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dirToTarget) < viewAng /2)
             {
-
-                if(flashlightInView[j].gameObject.CompareTag("flashlight"))
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+                if(!Physics.Raycast(transform.position, dirToTarget, distToTarget, Wall))
                 {
-                    visibleTargets.Add(flashlightInView[j].gameObject.transform);
+                    visibleTargets.Add(target);
                 }
+            }
+        }
 
-                Transform target = targetsInView[i].transform;
-                Vector3 dirToTarget = (target.position - transform.position).normalized;
-                if(Vector3.Angle(transform.forward, dirToTarget) < viewAng /2)
-                {
-                    float distToTarget = Vector3.Distance(transform.position, target.position);
-                    if(!Physics.Raycast(transform.position, dirToTarget, distToTarget, Wall))
-                    {
-                        visibleTargets.Add(target);
-                    }
-                }
+        Collider[] flashlightInView = Physics.OverlapSphere(new Vector3(transform.position.x, transform.position.y+1, transform.position.z), flashLightRad, targetMask);
+        for (int j = 0; j < flashlightInView.Length; j++)
+        {
+            if (flashlightInView[j].gameObject.CompareTag("flashlight"))
+            {
+                visibleTargets.Add(flashlightInView[j].gameObject.transform);
+                break;
             }
         }
     }
@@ -112,14 +112,14 @@ public class EnemySeek : MonoBehaviour
         Gizmos.color = Color.black;
         Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), flashLightRad);
     }
-    public Vector3 DirectionFromAng(float angInDeg, bool angleIsGlobal)
-    {
-        if(!angleIsGlobal)
-        {
-            angInDeg += transform.eulerAngles.y;
-        }
-        return new Vector3(Mathf.Sin(angInDeg * Mathf.Deg2Rad), 0, Mathf.Cos(angInDeg * Mathf.Deg2Rad)); 
-    }
+    //public Vector3 DirectionFromAng(float angInDeg, bool angleIsGlobal)
+    //{
+    //    if(!angleIsGlobal)
+    //    {
+    //        angInDeg += transform.eulerAngles.y;
+    //    }
+    //    return new Vector3(Mathf.Sin(angInDeg * Mathf.Deg2Rad), 0, Mathf.Cos(angInDeg * Mathf.Deg2Rad)); 
+    //}
 
 
     IEnumerator FindTargetDelayed(float delay)
