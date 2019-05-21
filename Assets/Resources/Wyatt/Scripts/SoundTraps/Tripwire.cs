697039b2202c2fc652eb231938f7afca688e6d69
Wyatt Gallagher <s179053@students.aie.edu.au>
2019-05-21 16:53:20 -0700

    sound fade

--- Assets/Resources/Wyatt/Scripts/SoundTraps/Tripwire.cs
+++ Assets/Resources/Wyatt/Scripts/SoundTraps/Tripwire.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Tripwire : MonoBehaviour
 {
-    //public Transform endOne;
-    //public Transform endTwo;
-
-    
-
     public SphereCollider soundCollider;
     public float killTime = .5f;
+
+    public GameObject broken;
     GameObject soundPos;
+
+
 
     public AudioClip chimes;
     public AudioClip snap;
     AudioSource source;
+
+
+
     public controler control;
     bool coroutineStarted;
     bool hasTriggered;
-
 
     void Start()
     {
@@ -57,6 +58,7 @@ public class Tripwire : MonoBehaviour
             source.PlayOneShot(snap);
             if (!coroutineStarted)
             {
+                Instantiate(broken, new Vector3(transform.position.x, .5f, transform.position.z), new Quaternion(Quaternion.identity.x, Random.rotation.y, Quaternion.identity.z, Quaternion.identity.w));
                 StartCoroutine(waitfordelete(spawnedCollider));
             }
         }
